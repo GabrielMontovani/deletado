0320f1232e2a1d2f92805ec9982e04916dc1c7fe
Gabriel <61791016+GabrielMontovani@users.noreply.github.com>
2021-10-10 12:35:27 -0300

    Deleete
    * * * * *

--- walle04/challenge_2Sprint/src/main/java/br/com/fiap/challenge/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.fiap.challenge.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.fiap.challenge.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
